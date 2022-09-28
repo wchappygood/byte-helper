33e9e25f53031b53c6322ba9dbc22be589e31795
songjun <songjun@jiuaoedu.com>
2022-09-28 15:13:15 +0800

    通过环境变量读取branchOrTag

--- byte-helper-plugin/version-for-tomcat/src/main/java/org/example/bytehelper/agent/plugin/versionfortomcat/VersionInfoInterceptor.java
+++ byte-helper-plugin/version-for-tomcat/src/main/java/org/example/bytehelper/agent/plugin/versionfortomcat/VersionInfoInterceptor.java
@@ -7,6 +7,7 @@ import org.example.bytehelper.agent.plugin.interceptor.InstanceMethodsIntercepto
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * @author SongJun
@@ -15,29 +16,30 @@ import java.lang.reflect.Method;
  */
 public class VersionInfoInterceptor implements InstanceMethodsInterceptor {
     private static final String VERSION_URI = "/versionInfo";
+    private static final String VERSION_MARK_KEY = "branchOrTag";
 
     @Override
     public Object beforeMethod(Method method, Object[] allArguments, Class<?>[] argumentsTypes) {
         Object ret = null;
         Request request = (Request) allArguments[0];
         if(VERSION_URI.equals(request.getRequestURI())){
-            Response response = (Response) allArguments[1];
-            String agentArgs = System.getProperty("agentArgs");
-            if(agentArgs != null){
-                System.out.println("查询版本, 版本标记:"+agentArgs);
+            Map<String, String> osEnv = System.getenv();
+            if(osEnv.containsKey(VERSION_MARK_KEY)){
+                String branchOrTag = osEnv.get(VERSION_MARK_KEY);
+                System.out.println("查询版本, 版本标记:"+branchOrTag);
                 try {
+                    Response response = (Response) allArguments[1];
                     PrintWriter writer = response.getWriter();
-                    writer.print(agentArgs);
+                    writer.print(branchOrTag);
                     writer.close();
                     response.flushBuffer();
-                    ret = agentArgs;
+                    ret = branchOrTag;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else {
                 System.out.println("查询版本, 未获取到版本标记");
             }
-
         }
         return ret;
     }
